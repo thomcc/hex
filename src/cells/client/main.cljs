9001440ead72aec88816750372dab49b4adbc1db
thom <chiovolonit@gmail.com>
2012-03-29 00:59:55 -0400

    made clicking work and ended up redoing the whole drawing system

--- src/cells/client/main.cljs
+++ src/cells/client/main.cljs
@@ -20,7 +20,7 @@
 (def colors {:off "hsl(0, 0%, 27%)", :on "hsl(60,70%,45%)", :or "hsl(0,70%,45%)",
              :xor "hsl(120,70%,45%)", :line "black"})
 
-(defn set-fill [ctx col] (set! ctx.fillStyle (colors col)) ctx)
+(defn set-fill [ctx col] (set! ctx.fillStyle col) ctx)
 (defn set-stroke [ctx col] (set! ctx.strokeStyle (colors col)) ctx)
 (defn stroke [ctx] (.stroke ctx) ctx)
 (defn fill [ctx] (.fill ctx) ctx)
@@ -47,69 +47,81 @@
 
 
 (def cs (atom (array (array))))
-(def cell-size 50)
-(def cell-height (* cell-size (Math/sqrt 2.25)))
-(def cell-width (* cell-size (Math/sqrt 3)))
+(def cell-radius 40)
 
-(defn update-cells [w h] (swap! cs make-cells (/ w cell-width) (/ h cell-height)))
+(def cell-width (* cell-radius 2))
+(def cell-height (Math/floor (* cell-radius (Math/sqrt 3))))
 
-(defn draw-hex [ctx x y]
-  (.beginPath ctx)
-  (loop [i 0]
-    (when (< i 7)
-      (let [a (* (/ Math/PI 180) (- (* i 60) 90)),
-            xx (+ x (* cell-size (Math/cos a))),
-            yy (+ y (* cell-size (Math/sin a)))]
-        (if (== 0 i)
-          (.moveTo ctx xx yy)
-          (.lineTo ctx xx yy)))
-      (recur (inc i))))
-  (doto ctx .closePath))
+(def cell-side (* (/ 3 2) cell-radius))
 
-(defn draw-hexs [cvs cls]
-  (let [w (.-width cvs), h (.-height cvs), ctx (.getContext cvs "2d"), xhexes (/ w cell-width), yhexes (/ h cell-height)]
-    (loop [xx 0]
-      (when (>= xhexes xx)
-        (loop [yy 0]
-          (when (>= yhexes yy)
-            (let [val (aget (aget cls yy) xx)]
-              (-> ctx
-                  (set-fill val)
-                  (draw-hex (+ (if (even? yy) 0 (/ cell-width 2)) (* xx cell-width))
-                            (* yy cell-height) s)
-                  fill
-                  stroke
-                  ))
-            (recur (inc yy))))
-        (recur (inc xx))))))
+(def corner-dx [(/ cell-radius 2), cell-side cell-width cell-side (/ cell-radius 2) 0])
+(def corner-dy [0 0 (/ cell-height 2) cell-height cell-height (/ cell-height 2)])
+
+(def neighbor-di [0 1 1 0 -1 -1])
+(def neighbor-dj [[-1 -1 0 1 0 -1] [-1 0 1 1 1 0]])
+
+(defn get-cell-pos [i j] [(* i cell-side) (* cell-height (/ (+ (* 2 j) (mod i 2)) 2))])
+(defn get-corners [x y] (map (fn [cx cy] [(+ x cx) (+ y cy)]) corner-dx corner-dy))
+
+
+(defn get-cell-index [x y]
+  (let [ci (Math/floor (/ x cell-side))
+        cx (- x (* cell-side ci))
+        ty (- y (* (mod ci 2) (/ cell-height 2)))
+        cj (Math/floor (/ ty cell-height))
+        cy (- ty (* height cj))]
+    (if-not (> cx (Math/abs (- (/ cell-radius 2) (* cell-radius (/ cy cell-height))))) [ci cj]
+            [(dec ci), (+ cj (- (mod ci 2) (if (< cy (/ cell-height 2)) 0 1)))])))
+
+(defn draw-cell [ctx i j fill-col]
+  (let [[x y] (get-cell-pos i j), [[cx cy] & cs :as c] (get-corners x y)]
+    (doto ctx .beginPath (.moveTo cx cy))
+    (doseq [[cx cy] cs] (. ctx (lineTo cx cy)))
+    (doto ctx .closePath .stroke (set-fill fill-col) .fill)))
+
+(defn update-cells [w h] (swap! cs make-cells (/ w cell-height) (/ h cell-height)))
+
+(defn draw-hexes [canvas cls]
+  (let [ctx (. canvas (getContext "2d"))
+        hei (.-length cls)
+        wid (.-length (aget cls 0))]
+    (loop [j 0]
+      (when (< j hei)
+        (loop [i 0]
+          (when (< i wid)
+            (draw-cell ctx i j (colors (aget (aget cls j) i)))
+            (recur (inc i))))
+        (recur (inc j))))))
+
+(def $canvas ($ :#canvas))
+(def canvas (.get $canvas 0))
+
+(defn repaint [] (draw-hexes canvas @cs))
 
 (defn on-resize []
-  (let [canvas (.get ($ :#canvas) 0)
-        $wind ($ js/window)
-        w (.width $wind)
-        h (.height $wind)]
+  (let [$wind ($ js/window), w (.width $wind), h (.height $wind)]
     (set! canvas.width w)
     (set! canvas.height h)
     (update-cells w h)
-    (draw-hexs canvas @cs)))
+    (repaint)))
 
-;; (defn on-click [e]
-;;   (let [$canvas ($ :#canvas)
-;;         ex (.getX e)
-;;          ey (.getY e)]
-;;     ()))
+(defn on-move [e]
+  (let [px (.-pageX e), py (.-pageY e), [i j] (get-cell-index px py), cur (aget (aget @cs j) i)]
+    (aset (aget @cs j) i (if (= :on cur) :off :on))
+    (repaint)))
+
 
 (defn init []
   (bind ($ js/window) :resize on-resize)
-  (bind ($ :#canvas) :click on-click)
+  (bind $canvas :mousedown (fn [e] (bind $canvas :mousemove on-move)))
+  (bind $canvas :mouseup (fn [e] (.unbind $canvas :mousemove)))
   (on-resize))
 
 
 (init)
 
-;(draw-hexs canvas 50)
 
-;(draw-hex ctx 50 50 50)
+
 
 
 
