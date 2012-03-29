3dce2ecbde33e729b3dbd56b2c1c1067ea189f2f
thom <chiovolonit@gmail.com>
2012-03-29 15:20:05 -0400

    fixed right-side clicking bug

--- src/cells/client/main.cljs
+++ src/cells/client/main.cljs
@@ -21,7 +21,7 @@
 (def cell-radius 40)
 (def cell-width (* cell-radius 2))
 (def cell-height (Math/floor (* cell-radius (Math/sqrt 3))))
-(def cell-side (* (/ 3 2) cell-radius))
+(def cell-side (* 1.5 cell-radius))
 
 (def corner-dx [(/ cell-radius 2), cell-side cell-width cell-side (/ cell-radius 2) 0])
 (def corner-dy [0 0 (/ cell-height 2) cell-height cell-height (/ cell-height 2)])
@@ -34,18 +34,16 @@
 (defn ns-selector [which] (fn [i j] (map #(neighbor i j %) which)))
 
 (def neighbors (ns-selector (range 6)))
-(def outputs   (ns-selector [0 2 4]))
-(def inputs    (ns-selector [1 3 5]))
+(def outputs (ns-selector [0 2 4]))
+(def inputs (ns-selector [1 3 5]))
 
 (defn hex-loc [i j] [(* i cell-side) (* cell-height (/ (+ (* 2 j) (mod i 2)) 2))])
 
 (defn hex-at [x y]
-  (let [ci (Math/floor (/ x cell-side)),
-        ty (- y (* (mod ci 2) (/ cell-height 2))),
-        cj (Math/floor (/ ty cell-height))
-        cx (- x (* cell-side ci)), cy (- ty (* height cj))]
-    (if-not (> cx (Math/abs (- (/ cell-radius 2) (* cell-radius (/ cy cell-height))))) [ci cj]
-            [(dec ci), (+ cj (- (mod ci 2) (if (< cy (/ cell-height 2)) 0 1)))])))
+  (let [ci (Math/floor (/ x cell-side)),   cx (- x (* cell-side ci)), ty (- y (* (mod ci 2) (/ cell-height 2))),
+        cj (Math/floor (/ ty cell-height)) cy (- ty (* cell-height cj))]
+    (if (> cx (* cell-radius (Math/abs (- 0.5 (/ cy cell-height))))) [ci cj]
+        [(dec ci), (+ cj (- (mod ci 2) (if (< cy (/ cell-height 2)) 1 0)))])))
 
 (defn draw-cell [ctx i j fill-col]
   (let [[x y] (hex-loc i j), [[cx cy] & cs] (get-corners x y)]
@@ -69,7 +67,7 @@
   (reduce (fn [cm [ni nj :as p]] (assoc cm p (next-cell (get-cell cm ni nj)))) cm (inputs i j)))
 
 (let [$canvas ($ :#canvas), canvas (.get $canvas 0), $window ($ js/window), hexes (atom {})]
-  (bind $canvas :click (fn [e] (let [pos (hex-at (.-pageX e) (.-pageY e)), current (or (@hexes pos) :off)]
+  (bind $canvas :click (fn [e] (let [pos (hex-at (.-pageX e) (.-pageY e)),current (apply get-cell @hexes pos)]
                                  (swap! hexes assoc pos (next-cell current))
                                  (swap! hexes toggle-neighbors pos)
                                  (draw canvas @hexes))))
@@ -77,6 +75,3 @@
                                     (set! canvas.height (.height $window))
                                     (draw canvas @hexes))))
   (trigger $window :resize))
-
-
-
